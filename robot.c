bba4ef746b44d2074bdd5128fcbd90436aec56bf
Philip Robinson <robinsph21@up.edu>
2018-12-06 17:05:08 -0800

    Committing again I guess

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake
+ * @author Tanya L. Crenshaw, Nathan Schmedake, Isaac Reinhard
  * @since August 2013
  *
  */
@@ -35,10 +35,17 @@ void robotPrintAscii(void)
   blank = blank +30;
   printf("\n\n"
 	 "UWR: Unidentified Wheeled Robot\n"
+<<<<<<< HEAD
 	 "%s      i_i    \n"
 	 "%s     [@_@]   \n"
 	 "%s  \\_/|___|\\_/ \n"
 	 "%s     d   b   \n",blank, blank, blank, blank);
+=======
+	 "%s    l_l    \n"
+	 "%s   [@_@]   \n"
+	 "%s  /|___|\\ \n"
+	 "%s   d   b   \n",blank, blank, blank, blank);
+>>>>>>> 8a9860156d96a8dbd5968a333af8f27bd7de8a5d
 
   blank = blank - 10;
 }
